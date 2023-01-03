786812ce587414d36137ce4ab9f9007e5115b615
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-01-04 06:43:51 +0700

    app

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = AutoTokenizer.from_pretrained("indolem/indobert-base-uncased")
+    model = BertForSequenceClassification.from_pretrained('indolem/indobert-base-uncased')
+    return tokenizer,model
+
+
+tokenizer,model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button("Analyze")
+
+d = {
+    
+  1:'HateSpeech',
+  0:'Non HateSpeech'
+}
+
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
