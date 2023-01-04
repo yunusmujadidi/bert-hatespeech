59528a5a3b7fd1cef61959feaa43ccc2416aadac
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-01-04 07:29:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ if user_input and button :
     output = model(**test_sample)
     # st.write("Logits: ",output.logits)
     y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
-    st.write("Prediction: ",d[y_pred[0]])
+    st.write("Prediksi: ",d[y_pred[0]])
