df105de9785f00d36799549d2da3cfd0194926e1
yunusmujadidi <62595282+yunusmujadidi@users.noreply.github.com>
2023-04-06 20:14:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,34 @@ def get_model():
 
 tokenizer,model = get_model()
 
+np.random.seed(42)
+if "cm" not in st.session_state:
+    st.session_state.cm = {}
+if "acc" not in st.session_state:
+    st.session_state.acc = {}
+judul = "Optimasi Algoritma Backpropagation untuk Klasifikasi Customer Churn dengan BFPA dan ENN"
+st.set_page_config(
+    initial_sidebar_state="expanded",
+    page_title=judul,
+    layout="centered",
+    page_icon="random",
+)
+
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.image("images/unnes.png")
+st.markdown(
+    "<h1 style='text-align: center'>Skripsi</h1>", unsafe_allow_html=True
+)
+st.markdown(
+    f"<h1 style='text-align: center'>{judul}</h1>",
+    unsafe_allow_html=True,
+)
+st.markdown(
+    "<h2 style='text-align: center'>Disusun oleh: Hatif Muhammad Fawwaz</h2>",
+    unsafe_allow_html=True,
+)
+
 user_input = st.text_area('Masukkan text yang ingin di prediksi')
 button = st.button("Prediksi")
 
