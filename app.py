7fe1d80f6d8df9471f2871954f5cf5b3bc5f5f8f
yunusmujadidi <62595282+yunusmujadidi@users.noreply.github.com>
2023-04-06 20:15:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,16 +43,3 @@ st.markdown(
 user_input = st.text_area('Masukkan text yang ingin di prediksi')
 button = st.button("Prediksi")
 
-d = {
-    
-  1:'HateSpeech',
-  0:'Non HateSpeech'
-}
-
-if user_input and button :
-    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
-    # test_sample
-    output = model(**test_sample)
-    # st.write("Logits: ",output.logits)
-    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
-    st.write("Prediksi: ",d[y_pred[0]])
